b5d9f454faeef815874f75b1ac520d126abe250f
Yura Khalus <36310589+WebUraXalys@users.noreply.github.com>
2022-11-04 11:08:13 +0000

    new

--- lab2/C++/Sort.cpp
+++ lab2/C++/Sort.cpp
@@ -1,76 +1,80 @@
 #include <iostream>
-#include <ctime>
-#include <vector>
-#include "Sort.h"
+#include <cstdlib>
 
 
+using namespace std;
 
-	static void Sort::Show(const int arr[], int size)
-	{
-		for (int i = 0; i < size; i++)
-		{
-			std::cout << arr[i] << " ";
+void show(int *A, int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        cout << A[i] << " ";
+    }
+    cout << "\n\n";
+}
+void SWAP(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+// базовий масив
+void New_Arr(int *A, int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+       A[i] = 9 - i;
+    }
+    cout << "Array: ";
+    show(A, n);
+}
+
+void bubble_sort(int *A, int n, int st, int en)
+{
+      for (int i = st-1; i < en-1; i++)
+        for (int j = st-1; j < en-1; j++)
+            if (A[j]>A[j+1]) SWAP(A[j],A[j+1]);
+    cout << "Bubble: ";
+    show(A, n);
+}
+
+void insertion_sort (int *A, int n, int st, int en)
+{
+    for (int i = st-1; i <en; i++)
+    {
+        int key = A[i];
+		int b = i;
+		while ((b > st-1) && A[b - 1] > key)
+        {
+			A[b] = A[b - 1];
+			b--;
 		}
-		std::cout << std::endl;
+		A[b] = key;
+    }
+    cout << "Insertion: ";
+    show(A, n);
+}
+
+int MinItem(int *A, int k, int n)
+{
+	int min_a = k;
+	while (k < n)
+    {
+        if (A[min_a] >= A[k])
+        {
+            min_a = k;
+        }
+        k++;
 	}
+	return min_a;
+}
+void selection_sort(int *A, int n, int st, int en)
+{
+    for(int i = st-1; i < en; i++)
+    {
+        SWAP(A[MinItem(A, i, en)], A[i]);
+    }
+    cout << "Selection: ";
+    show(A, n);
+}
 
-	static void Sort::SortBubble(int arr[], const int start_index, const int end_index) { 	//бульбашка
-		for (int i = start_index; i < end_index; i++)
-			for (int j = end_index; j > i; j--)
-				if (arr[j] < arr[j - 1])
-					std::swap(arr[j], arr[j - 1]);
-
-	}
-
-
-	static void Sort::SortInsertion(int arr[], const int start_index, const int end_index) //вставка
-	{
-		int i, j, key;
-		for (i = start_index; i <= end_index; i++)
-		{
-			key = arr[i];
-			j = i - 1;
-			while (j >= start_index && arr[j] > key)
-			{
-				arr[j + 1] = arr[j];
-				j = j - 1;
-			}
-			arr[j + 1] = key;
-		}
-	}
-
-	static void Sort::SortSelection(int arr[], const int start_index, const int end_index) { //вибірка
-		for (int i = start_index; i < end_index; i++)
-			for (int j = i + 1; j < end_index + 1; j++)
-				if (arr[j] < arr[i])
-					Sort::swap(arr[j], arr[i]);
-	}
-
-	static void Sort::MinItem(int arr[], int size) {
-		int minValue = arr[0];
-		for (int i = 0; i < size; i++)
-		{
-			if (arr[i] < minValue) {
-				minValue = arr[i];
-			}
-		}
-		std::cout << "Element with a minimum value - " << minValue << std::endl;
-	}
-
-	static void Sort::MaxItem(int arr[], int size) {
-		int maxValue = arr[0];
-		for (int i = 0; i < size; i++)
-		{
-			if (arr[i] > maxValue) {
-				maxValue = arr[i];
-			}
-		}
-		std::cout << "Element with a maximum value - " << maxValue << std::endl;
-	}
-
-
-	static void Sort::swap(int& a, int& b) {
-		int temp = a;
-		a = b;
-		b = temp;
-	}
